9048d7587c6004c7c44eca3a05739cbb5cb20ecb
Abhinay Pandey <abhinaypandey02@gmail.com>
2021-08-10 23:24:27 +0530

    chat

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as LIBRARY_JSON from './library.json';
 import Queue from "./classes/Queue";
 import { Server, Socket } from "socket.io";
+import {Chat} from "./interfaces/Chat";
 
 const io = new Server(3001,{
     cors:{
@@ -10,9 +11,17 @@ const io = new Server(3001,{
 const queue=new Queue(LIBRARY_JSON.library);
 queue.next();
 
+let chats:Chat[]=[];
+
 io.on("connection",(socket:Socket)=>{
-    console.log(socket.id);
     socket.on("askSong",()=> {
         socket.emit("receiveSong",queue.currentSong,queue.currentState,queue.nextSong);
     });
+    socket.on("sendChat",(chat:Chat)=>{
+        chats.push(chat);
+        socket.emit("newChat",chats);
+    })
+    socket.on("getChats",()=>{
+        socket.emit("newChat",chats);
+    })
 })
---
+++ interfaces/Chat.ts
@@ -0,0 +1,6 @@
+export interface Chat{
+    sender:string;
+    message:string;
+    id:string;
+    time:string;
+}
